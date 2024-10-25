95dd63db48dc4c6deece483cddd25d19aad0fd8f
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2024-10-25 14:09:33 +0100

    Edited mlp.py, requirements.txt and updated readme. Added folders to aid in clarity

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Comp516
-Group project repo for Comp516 research project at University of Liverpool
+# Comp516 Msc Project #
+## A Convolutional Neural Network Approach to Gravitational Wave Data Analysis ##
+Group 4: 
+
+## Introduction ##
+
+The objective of this project is to develop a model capable of classifying an asteroids taxonomy and family based on spectral and orbital data.
+
+To classify asteroids, we developed both an MLP and Extra Trees model. Since the MLP preformed best on spectral data and Extra Trees performed best on family data. 
+
+## Packages Used ##
+
+Please see [`requirements.txt`](https://github.com/mlbuiten515/Comp516/blob/main/requirements.txt) for a complete list of packages used in this project.
+
---
+++ src/mlp.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------------------------------------------------------------
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Activation, BatchNormalization, Dense, Dropout, ReLU, Softmax
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Define Multi-Layer Perceptron
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def MLP():
+    """
+    Multi-Layer Perceptron for the classification of Asteroid Taxonomy and Family.
+
+    Structure
+    ---------
+    Input
+    """
+    model = Sequential()
+
+    model.add()
+
+    return model
